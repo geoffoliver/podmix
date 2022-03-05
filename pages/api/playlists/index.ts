79e1cd0fd7ce9fbd9d5b6d002379de15755147fe
Geoff Oliver <geoff@plan8studios.com>
2022-03-04 22:21:29 -0700

    removed console log

--- pages/api/playlists/index.ts
+++ pages/api/playlists/index.ts
@@ -36,7 +36,5 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     order: [['createdAt', 'DESC']],
   });
 
-  console.log(JSON.stringify(playlists, null, 2));
-
   return res.status(200).json({ playlists })
 }
