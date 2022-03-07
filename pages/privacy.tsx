f9aa4f526049b7fefca49650d4075271274158ee
Geoff Oliver <geoff@plan8studios.com>
2022-03-06 20:28:09 -0700

    add title to terms and privacy pages

--- pages/privacy.tsx
+++ pages/privacy.tsx
@@ -1,23 +1,28 @@
 import { Container, Row, Col } from 'react-bootstrap';
 
+import Head from '@/components/Head';
+
 const Privacy = () => {
   return (
-    <Container>
-      <Row>
-        <Col className="pt-4" xl={{ span: 6, offset: 3 }}>
-          <h1>Privacy Policy</h1>
-          <p>
-            We respect your privacy. We don&apos;t sell your data, and we don&apos;t track you with ads or other sketchy
-            stuff. We only store your profile information, your favorites, and your playlists, because that&apos;s all
-            we need!
-          </p>
-          <h3>Deleting your data</h3>
-          <p>
-            If you want to delete your data from Podmix, just send an email to <a href="mailto:data@podmix.me">data@podmix.me</a>.
-          </p>
-        </Col>
-      </Row>
-    </Container>
+    <>
+      <Head title="Privacy Policy" />
+      <Container>
+        <Row>
+          <Col className="pt-4" xl={{ span: 6, offset: 3 }}>
+            <h1>Privacy Policy</h1>
+            <p>
+              We respect your privacy. We don&apos;t sell your data, and we don&apos;t track you with ads or other sketchy
+              stuff. We only store your profile information, your favorites, and your playlists, because that&apos;s all
+              we need!
+            </p>
+            <h3>Deleting your data</h3>
+            <p>
+              If you want to delete your data from Podmix, just send an email to <a href="mailto:data@podmix.me">data@podmix.me</a>.
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 };
 
--- pages/terms.tsx
+++ pages/terms.tsx
@@ -1,30 +1,35 @@
 import { Container, Row, Col } from 'react-bootstrap';
 
+import Head from '@/components/Head';
+
 const Terms = () => {
   return (
-    <Container>
-      <Row>
-        <Col className="pt-4" xl={{ span: 6, offset: 3 }}>
-          <h1>Terms Of Use</h1>
-          <p>
-            The service (&ldquo;Podmix&rdquo;) is provided as-is and offers no warranty or guarantees on service
-            availability, reliability, or security.
-          </p>
-          <p>
-            All podcast data is provided by a third party and Podmix will not be held responsible for incorrect information.
-          </p>
-          <p>
-            Podmix may restrict access, disable, or delete an account without notice.
-          </p>
-          <p>
-            Podmix may delete data without notice.
-          </p>
-          <p>
-            There really isn&apos;t much more to it. This is is a free service that lets you make playlists from podcast episodes. Enjoy!
-          </p>
-        </Col>
-      </Row>
-    </Container>
+    <>
+      <Head title="Terms of Use" />
+      <Container>
+        <Row>
+          <Col className="pt-4" xl={{ span: 6, offset: 3 }}>
+            <h1>Terms Of Use</h1>
+            <p>
+              The service (&ldquo;Podmix&rdquo;) is provided as-is and offers no warranty or guarantees on service
+              availability, reliability, or security.
+            </p>
+            <p>
+              All podcast data is provided by a third party and Podmix will not be held responsible for incorrect information.
+            </p>
+            <p>
+              Podmix may restrict access, disable, or delete an account without notice.
+            </p>
+            <p>
+              Podmix may delete data without notice.
+            </p>
+            <p>
+              There really isn&apos;t much more to it. This is is a free service that lets you make playlists from podcast episodes. Enjoy!
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 };
 
